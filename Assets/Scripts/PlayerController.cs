1a4c4d0313257087dd3cd75238b36af976235f7a
YonatanSokolov <yoni.sokolov@gmail.com>
2023-10-26 17:47:19 +0300

    Fixed movement, jumping and physics issues

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,52 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody rb;
     float delta;
-    public float speed = 120f;
-    public float jump_speed = 100f;
-    public bool IsGrounded = true;
-    public bool testbool = true;
+    Vector3 Curr_velocity;
+    private float speed = 15f;
+    private float jump_speed = 30f;
+    private bool IsGrounded = true;
+    Vector3 moveDirection;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Curr_velocity = Vector3.zero;
+        Physics.gravity = new Vector3 (0,-100,0);
     }
+
     void Update()
     {
-        delta = Time.deltaTime;
-        float xDirection = Input.GetAxis("Horizontal");
-        float zDirection = Input.GetAxis("Vertical");
-
-        Vector3 moveDirection = new Vector3(xDirection, 0.0f, zDirection);
-        rb.velocity = moveDirection;
         if (Input.GetButtonDown("Jump") && IsGrounded)
         {
-            //rb.isKinematic = false;
-            //rb.AddForce(new Vector3(0.0f, jump_speed, 0.0f), ForceMode.Impulse);
-            rb.velocity = rb.velocity + Vector3.up * jump_speed;
+            rb.AddForce(new Vector3(0.0f, jump_speed, 0.0f), ForceMode.Impulse);
             IsGrounded = false;
         }
-        if (Input.GetKeyDown(KeyCode.T) && testbool == true) 
-        {
-            testbool = false;
-            rb.position += new Vector3(12*delta , 0.0f, 0.0f);
-        }
-
-        
     }
-    
+    void FixedUpdate()
+    {
+        delta = Time.deltaTime;
+        float xDirection = Input.GetAxis("Horizontal")*speed;
+        float zDirection = Input.GetAxis("Vertical")*speed;
+
+        rb.velocity = new Vector3(xDirection,rb.velocity.y,zDirection);
+
+
+        //moveDirection = new Vector3(xDirection, 0.0f, zDirection) * speed * delta;
+        //rb.MovePosition(rb.position + moveDirection);
+        //rb.AddForce(moveDirection, ForceMode.VelocityChange);
+    }
+
+    /*private void FixedUpdate()
+    {
+        transform.position += moveDirection;
+    }*/
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "Ground") // update the collision with ground bool
         {
-            if (rb.velocity.y <= 0)
-            {
-                //rb.isKinematic = true;
-            }
             IsGrounded = true;
         }
     }
